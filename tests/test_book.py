54fee109af9a778efd0bfd908b43fa0565e6d19d
yprokhoda <yprokhoda@griddynamics.com>
2021-07-28 13:39:38 +0300

    Update test description;

--- tests/test_book.py
+++ tests/test_book.py
@@ -14,9 +14,9 @@ def test_add_book(book):
     res = add_book(book.type, book.title, book.date)
     print(res)
     res = json.loads(res)
-    assert res['title'] == book.title, "should be equal"
-    assert res['type'] == book.type, "should be equal"
-    assert res['creation_date'] == book.date, "should be equal"
+    assert res['title'] == book.title, f"Title verification: expected: {book.title}, actual: {res['title']}"
+    assert res['type'] == book.type, f"Type verification: expected: {book.type}, actual: {res['type']}"
+    assert res['creation_date'] == book.date, f"Type verification: expected: {book.date}, actual: {res['type']}"
 
 
 @pytest.mark.parametrize('book', [book1, book2])
@@ -27,9 +27,9 @@ def test_delete_book(book):
 
     res = delete_book(id=res_add_id)
     res = json.loads(res)
-    assert res['title'] == book.title, "should be equal"
-    assert res['type'] == book.type, "should be equal"
-    assert res['creation_date'] == book.date, "should be equal"
+    assert res['title'] == book.title, f"Title verification: expected: {book.title}, actual: {res['title']}"
+    assert res['type'] == book.type, f"Type verification: expected: {book.type}, actual: {res['type']}"
+    assert res['creation_date'] == book.date, f"Type verification: expected: {book.date}, actual: {res['type']}"
 
 
 @pytest.mark.parametrize('book', [book1, book2])
@@ -43,10 +43,10 @@ def test_update_book(book):
     res = update_book(id=res_add_id, type=changed_type, title=changed_title)
     res = json.loads(res)
 
-    assert res['id'] == res_add_id, "should be equal"
-    assert res['type'] == changed_type, "should be equal"
-    assert res['title'] == changed_title, "should be equal"
-    assert res['creation_date'] == book.date, "should be equal"
+    assert res['id'] == res_add_id, f"ID verification: expected: {res_add_id}, actual: {res['id']}"
+    assert res['type'] == changed_type, f"Type verification: expected: {changed_type}, actual: {res['type']}"
+    assert res['title'] == changed_title, f"Title verification: expected: {changed_title}, actual: {res['title']}"
+    assert res['creation_date'] == book.date, f"Type verification: expected: {changed_type}, actual: {res['type']}"
 
 
 @pytest.mark.parametrize('book', [book1, book2])
@@ -54,9 +54,10 @@ def test_get_book_by_type(book):
     add_book(book.type, book.title, book.date)
     res = get_book_by_type(type=book.type)
     res = json.loads(res)
-    assert res[-1]['title'] == book.title, "should be equal"
-    assert res[-1]['type'] == book.type, "should be equal"
-    assert res[-1]['creation_date'] == book.date, "should be equal"
+    assert res[-1]['title'] == book.title, f"Title verification: expected: {book.title}, actual: {res[-1]['title']}"
+    assert res[-1]['type'] == book.type, f"Type verification: expected: {book.type}, actual: {res[-1]['type']}"
+    assert res[-1]['creation_date'] == book.date, f"Creation date verification: expected: {book.date}, " \
+                                                  f"actual: {res[-1]['creation_date']}"
 
 
 @pytest.mark.parametrize('book', [book1, book2])
@@ -68,9 +69,10 @@ def test_get_book_by_id(book):
     res = get_book_by_id(id=res_add_id)
     res = json.loads(res)
 
-    assert res['title'] == book.title, "should be equal"
-    assert res['type'] == book.type, "should be equal"
-    assert res['creation_date'] == book.date, "should be equal"
+    assert res['title'] == book.title, f"Title verification: expected: {book.title}, actual: {res['title']}"
+    assert res['type'] == book.type, f"Type verification: expected: {book.type}, actual: {res['type']}"
+    assert res['creation_date'] == book.date, f"Creation date verification: expected: {book.date}, " \
+                                              f"actual: {res['creation_date']}"
 
 
 def test_get_book_latest():
@@ -81,10 +83,12 @@ def test_get_book_latest():
     res = json.loads(res)
     pprint(res)
 
-    assert res[0]['title'] == book2.title, "should be equal"
-    assert res[0]['type'] == book2.type, "should be equal"
-    assert res[0]['creation_date'] == book2.date, "should be equal"
+    assert res[0]['title'] == book2.title, f"Title verification: expected: {book2.title}, actual: {res[0]['title']}"
+    assert res[0]['type'] == book2.type, f"Type verification: expected: {book2.type}, actual: {res[0]['type']}"
+    assert res[0]['creation_date'] == book2.date, f"Creation date verification: expected: {book2.date}, " \
+                                                  f"actual: {res[0]['creation_date']}"
 
-    assert res[1]['title'] == book1.title, "should be equal"
-    assert res[1]['type'] == book1.type, "should be equal"
-    assert res[1]['creation_date'] == book1.date, "should be equal"
+    assert res[1]['title'] == book1.title, f"Title verification: expected: {book1.title}, actual: {res[1]['title']}"
+    assert res[1]['type'] == book1.type, f"Type verification: expected: {book1.type}, actual: {res[1]['type']}"
+    assert res[1]['creation_date'] == book1.date, f"Creation date verification: expected: {book1.date}, " \
+                                                  f"actual: {res[1]['creation_date']}"
